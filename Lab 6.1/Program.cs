121112c2187f7f16b85890b611b69bd555927487
makarevichdv <makarevichdv@MAKAREVICH>
2021-11-29 17:35:10 +0300

    Добавьте файлы проекта.

---
+++ Lab 6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string s = Console.ReadLine();
+            string wordMaxLength = string.Empty;
+            string[] words = s.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                if (words[i].Length > wordMaxLength.Length)
+                    wordMaxLength = words[i];
+            Console.WriteLine(wordMaxLength);
+            Console.ReadLine();
+
+        }
+    }
+}
